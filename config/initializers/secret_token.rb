6d9c5141540ddf9761d795cbfb2825120001343b
dharasydnia <djoshi@sydnia.com>
2013-03-08 17:18:15 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gitrepotestnew::Application.config.secret_token = '6be3070f7728eb800837c4eb5271af8030b6db32ff8c1a107a9705f3483aef11af4be0ee8c68df44b55ded890bd99222fdd5fc997dcb44fccffc36e9f3fbdf76'
